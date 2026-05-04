65b0915ec56391f4580257dbe64db71762ae7d6d
Liangwei-zhang <2876252@gmail.com>
2026-05-03 20:14:07 -0600

    feat: add logging configuration

---
+++ app/logging_config.py
@@ -0,0 +1,14 @@
+import sys
+from loguru import logger
+
+
+def configure_logging(level: str = "INFO") -> None:
+    """Configure application logging."""
+
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        level=level.upper(),
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        enqueue=True,
+    )
